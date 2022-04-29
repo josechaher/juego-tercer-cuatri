ac680ab362a3e7c36fb28809831cfe1487332c5d
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2022-04-29 10:49:14 -0300

    Muchos cambios

---
+++ juego tercer cuatrimestre/Assets/_Scripts/Collector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Collector : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.GetComponent<Player>()!=null)
+        { 
+            GetComponent<AudioSource>().Play();
+            Destroy(this.gameObject, 0.5f);
+        }
+       
+    }
+    
+    private void Update()
+    {
+        transform.localEulerAngles += new Vector3(0, 1, 0) * 40 * Time.deltaTime;
+    }
+}
