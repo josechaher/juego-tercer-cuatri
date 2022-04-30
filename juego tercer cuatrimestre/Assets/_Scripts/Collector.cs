16fe895622bc61549f3228e558c47c8f01bc0cd5
josechaher <102690878+josechaher@users.noreply.github.com>
2022-04-30 20:14:50 -0300

    Seals
    Fixed seals and prefabed them

--- juego tercer cuatrimestre/Assets/_Scripts/Collector.cs
+++ juego tercer cuatrimestre/Assets/_Scripts/Collector.cs
@@ -6,10 +6,10 @@ public class Collector : MonoBehaviour
 {
     public void OnTriggerEnter(Collider collision)
     {
-        if (collision.GetComponent<Player>()!=null)
+        if (collision.GetComponent<Player>() != null)
         { 
             GetComponent<AudioSource>().Play();
-            Destroy(this.gameObject, 0.5f);
+            Destroy(this.gameObject);
         }
        
     }
