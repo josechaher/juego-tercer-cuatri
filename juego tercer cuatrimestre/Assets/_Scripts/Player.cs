82cb6d4a6fdd7ade88416a950de0bf36902832f4
Roro1008 <62655472+Roro1008@users.noreply.github.com>
2022-05-09 02:11:26 -0300

    Finalmenthe

--- juego tercer cuatrimestre/Assets/_Scripts/Player.cs
+++ juego tercer cuatrimestre/Assets/_Scripts/Player.cs
@@ -17,6 +17,7 @@ public class Player : Entity
     // FALLING
 
     // CLIMBING
+    Player_Movement _move;
 
     public float PlayerHealth;
 
@@ -24,88 +25,80 @@ public class Player : Entity
 
     public Text debug;
 
+    //Movimiento
     Vector3 moveDirection;
-    [SerializeField] float moveSpeed = 5;
+    public float moveSpeed = 5;
+    public CharacterController cr;
 
+    //turn
     public Vector2 mousePosition;
-    [SerializeField] float lookSensitivity = 2;
+    public float lookSensitivity = 2;
 
+
+    //Salto
     [SerializeField] float jumpHeight = 5;
     public Vector3 velocity;
 
     [SerializeField] float gravity = -10;
 
-    [SerializeField] float distToGround;
+    float distToGround;
 
-    CharacterController cr;
 
-    [SerializeField] private Material handMaterial;
-    [SerializeField] private Renderer hand;
 
+    //Mano
+    [SerializeField] Material handMaterial;
+    [SerializeField] Renderer hand;
     [SerializeField] ParticleSystem handParticles;
 
-    bool isGrounded;
+    //Grounded
+    [SerializeField] bool isGrounded;
 
+    //emmissive esfera
     public bool glowing = false;
     public float glowIntensity = 0;
     Color color;
 
+    //esfera
     [SerializeField] Transform ballSpawn;
     [SerializeField] GameObject ballPrefab;
-    GameObject ball;
+    private GameObject ball;
 
+    //Animator
     [SerializeField] private Animator animator;
 
     private void Awake()
     {
         cr = GetComponent<CharacterController>();
-        distToGround = cr.height / 2;
-
     }
 
-    
+
     void Start()
     {
         handMaterial = hand.GetComponent<Renderer>().material;
         color = handMaterial.GetColor("_EmissionColor");
         Cursor.lockState = CursorLockMode.Locked;
+        distToGround = cr.height / 2;
         SetHealth(PlayerHealth);
+        _move = new Player_Movement(moveDirection, moveSpeed, cr, transform, mousePosition, lookSensitivity, jumpHeight, velocity, gravity, distToGround, isGrounded, debug);
     }
 
-    void Update()
+    // ArtificialUpdate is called on Entity's update function
+    protected override void ArtificialUpdate()
     {
-        #region MOVEMENT
-        //GRAVITY
-        velocity.y += gravity * Time.deltaTime;
+        #region function calls
 
-        cr.Move(velocity * Time.deltaTime);        
+        _move.MovementUpdate();
 
-        // PLAYER ROTATION
-        Turn();
-
-        // PLAYER WALK
-        Walk();
-
-        // RUN
-        if (Input.GetKeyDown(KeyCode.LeftShift)) moveSpeed *= 2;        
-        if (Input.GetKeyUp(KeyCode.LeftShift)) moveSpeed /= 2;
-
-        //GROUND CHECK
-        GroundCheck();
-
-        // JUMP
-        Jump();
         #endregion
-
         #region SHOOTING
         if (Input.GetMouseButtonDown(0))
         {
-            
+
             ball = Instantiate(ballPrefab, Camera.main.transform);
             ball.transform.position = ballSpawn.position;
 
             handParticles.Play();
-            
+
             glowIntensity = 0;
             handMaterial.EnableKeyword("_EMISSION");
             glowing = true;
@@ -133,73 +126,6 @@ public class Player : Entity
         {
             SceneManager.LoadScene("Level 1 - Roro");
         }
-    }
-
-    /// <summary>
-    /// Walk towards where player is facing
-    /// </summary>
-    void Walk()
-    {
-        moveDirection = transform.right * Input.GetAxisRaw("Horizontal")
-                    + transform.forward * Input.GetAxisRaw("Vertical");
-
-        cr.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
-    }
-
-    /// <summary>
-    /// Rotates player and camera according to mouse movements
-    /// </summary>
-    void Turn()
-    {
-        mousePosition.x += Input.GetAxisRaw("Mouse X") * lookSensitivity; 
-        mousePosition.x %= 360; // Snaps rotation angle back to 0 when it reaches 360 or -360 degrees
-
-        mousePosition.y -= Input.GetAxisRaw("Mouse Y") * lookSensitivity; 
-        mousePosition.y = Mathf.Clamp(mousePosition.y, -90, 90); // Prevents camera from rotating past its vertical axis
-
-        transform.rotation = Quaternion.Euler(Vector3.up * mousePosition.x);
-        Camera.main.transform.localRotation = Quaternion.Euler(Vector3.right * mousePosition.y);
-    }
-
-    void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            // Velocity is set so that player will reach desired jump height and then start falling
-            velocity.y = Mathf.Sqrt(-2 * gravity * jumpHeight);
-        }
-    }
-
-
-    void GroundCheck()
-    {
-        RaycastHit hit;        
-
-        // Checks Player's distance from the ground
-        if (Physics.SphereCast(transform.position, 0.5f, Vector3.down, out hit, distToGround - 0.41f))
-        {
-            // Ignore collisions in "Player" layer
-            if (!(hit.collider.gameObject.layer == 3))
-            {
-                isGrounded = true;
-                debug.text = "Grounded";
-                velocity.y = 0;
-            }
-            
-
-            
-        } else
-        {
-            isGrounded = false;
-            debug.text = "Not Grounded";
-        }
-    }
-
-    // ArtificialUpdate is called on Entity's update function
-    protected override void ArtificialUpdate()
-    {
-        // Makes slider face enemy
-        slider.transform.forward = Camera.main.transform.forward;
     }
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
---
+++ juego tercer cuatrimestre/Assets/_Scripts/Player_Movement.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Player_Movement 
+{
+    public Text debug;
+
+    //walk
+    Vector3 _moveDirection;
+    float _moveSpeed ;
+    CharacterController _cr;
+    Transform _t;
+    
+    //turn
+    public Vector2 _mousePosition;
+    float _lookSensitivity ;
+    
+
+    //salto
+    float _jh ; //jumpHeight
+    public Vector3 _v;              //velocity
+    float _g ;//gravity
+    float _dtg; //distToGround
+
+    bool _isGrounded;
+    // Start is called before the first frame update
+
+    public Player_Movement(Vector3 moveDirection, float moveSpeed, CharacterController cr, Transform t, Vector2 mousePosition, float lookSensitivity,
+                            float jumpHeight, Vector3 velocity, float gravity, float distToGround, bool isGrounded, Text debug)
+    {
+        _moveDirection = moveDirection;
+        _moveSpeed = moveSpeed;
+        _cr = cr;
+        _t = t;
+        _mousePosition = mousePosition;
+        _lookSensitivity = lookSensitivity;
+        _jh = jumpHeight;
+        _v = velocity;
+        _g = gravity;
+        _dtg = distToGround;
+        this._isGrounded = isGrounded;
+        this.debug = debug;
+
+    }
+
+    public void MovementUpdate()
+    {
+        //GRAVITY
+        GravityUpdate();
+
+        // PLAYER ROTATION
+        Turn();
+
+        // PLAYER WALK
+        Walk();
+
+        //GROUND CHECK
+        GroundCheck();
+
+        // JUMP
+        Jump();
+    }
+
+
+
+    public void Walk()
+    {
+        _moveDirection = _t.right * Input.GetAxisRaw("Horizontal")
+                    + _t.forward * Input.GetAxisRaw("Vertical");
+
+        _cr.Move(_moveDirection.normalized * _moveSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)) _moveSpeed *= 2;
+        if (Input.GetKeyUp(KeyCode.LeftShift)) _moveSpeed /= 2;     //esto esta rancio
+    }
+
+    /// <summary>
+    /// Rotates player and camera according to mouse movements
+    /// </summary>
+    
+    
+    public void Turn()
+    {
+        _mousePosition.x += Input.GetAxisRaw("Mouse X") * _lookSensitivity;
+        _mousePosition.x %= 360; // Snaps rotation angle back to 0 when it reaches 360 or -360 degrees
+
+        _mousePosition.y -= Input.GetAxisRaw("Mouse Y") * _lookSensitivity;
+        _mousePosition.y = Mathf.Clamp(_mousePosition.y, -90, 90); // Prevents camera from rotating past its vertical axis
+
+        _t.rotation = Quaternion.Euler(Vector3.up * _mousePosition.x);
+        Camera.main.transform.localRotation = Quaternion.Euler(Vector3.right * _mousePosition.y);
+    }
+
+    public void GravityUpdate()
+    {
+        _v.y += _g * Time.deltaTime;
+
+        _cr.Move(_v * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            Debug.Log("estoy en el jump");
+            // Velocity is set so that player will reach desired jump height and then start falling
+            _v.y = Mathf.Sqrt(-2 * _g * _jh);
+        }
+    }
+
+    public void GroundCheck()
+    {
+        RaycastHit hit;
+
+        Debug.Log("estoy en el grundcheck");
+
+        // Checks Player's distance from the ground
+        if (Physics.SphereCast(_t.position, 0.5f, Vector3.down, out hit, _dtg - 0.41f))
+        {
+            Debug.Log("estoy en el raycast");
+            // Ignore collisions in "Player" layer
+            if (!(hit.collider.gameObject.layer == 3))
+            {
+                _isGrounded = true;
+                debug.text = "Grounded";
+                _v.y = 0;
+            }
+
+
+
+        }
+        else
+        {
+            _isGrounded = false;
+            debug.text = "Not Grounded";
+        }
+    }
+
+}
