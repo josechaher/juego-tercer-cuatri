7fa005a9fb56328178eb0fe62e83445fde906e8a
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2022-05-27 12:21:54 -0300

    FallingPlatform Beginning

---
+++ di yu hell in kowloon/Assets/_Scripts/FallingPlatform.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    bool isFalling = false;
+
+    /*void OnTriggerEnter(Collider other)
+    {
+        if (Collider.gameObject.name == "Player")
+            isFalling = true;
+    }*/
+}
