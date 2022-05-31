30899b8b871769f47de72195088608f09c6e46f2
Roro1008 <62655472+Roro1008@users.noreply.github.com>
2022-05-30 21:50:34 -0300

    Level 2 touches

--- di yu hell in kowloon/Assets/_Scripts/FallingPlatform.cs
+++ di yu hell in kowloon/Assets/_Scripts/FallingPlatform.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     bool isFalling = false;
+    bool playerEntered = false;
+    float offsetFallTime = 2;
+    float downSpeed = 0;
 
-    /*void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider collider)
     {
-        if (Collider.gameObject.name == "Player")
-            isFalling = true;
-    }*/
+        if (collider.gameObject.name == "Player")
+            playerEntered = true;
+
+    }
+
+    void Update()
+    {
+        if (playerEntered)
+        {
+            offsetFallTime -= Time.deltaTime;
+            if (offsetFallTime <=0)
+            {
+                isFalling = true;
+            }
+        }
+
+        if (isFalling)
+        {
+            downSpeed += Time.deltaTime/10;
+            transform.position = new Vector3(transform.position.x, transform.position.y - downSpeed, transform.position.z);
+        }
+    }
 }
