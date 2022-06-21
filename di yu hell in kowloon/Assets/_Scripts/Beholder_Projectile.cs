c3efee1bf35c6e20366cd5e310901a3fa943f493
Roro1008 <62655472+Roro1008@users.noreply.github.com>
2022-06-21 01:03:33 -0300

    Level 3, Bala beholder y menu pausa

--- di yu hell in kowloon/Assets/_Scripts/Beholder_Projectile.cs
+++ di yu hell in kowloon/Assets/_Scripts/Beholder_Projectile.cs
@@ -18,7 +18,7 @@ public class Beholder_Projectile : MonoBehaviour
     private void Update()
     {
         transform.LookAt(player);
-        transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime*4);
         
     }
 
